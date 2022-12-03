a82152824eb753d12e719f97a5e27e1b3e23438f
kaushik2107-bit <kaushiksbhowmick@gmail.com>
2022-12-03 10:01:57 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,199 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const multer = require('multer')
+const { Readable } = require("stream")
+const app = express()
+const router = express.Router()
+const PORT = process.env.PORT
+app.use(express.json())
+app.use(cors())
+
+const mongodb = require("mongodb")
+const MongoClient = require('mongodb').MongoClient
+const ObjectID = require('mongodb').ObjectID
+
+// connecting to the database
+let db
+MongoClient.connect(process.env.DB, (err, client) => {
+  if (err) {
+    console.log(err)
+    process.exit(1)
+  }
+
+  db = client.db('musicplayer')
+  console.log("Database Connected Successfully")
+})
+
+
+// post route of  a song
+app.post('/tracks', (req, res) => {
+
+  const storage = multer.memoryStorage()
+  const upload = multer({
+    storage: storage,
+  }).fields([
+    {
+      name: 'track',
+      maxCount: 1
+    },
+    {
+      name: 'image',
+      maxCount: 1
+    }
+  ])
+  upload(req, res, (err) => {
+    console.log(req.files['track'][0])
+    // return res.status(202).send({message: 'ok'})
+    if (err) {
+      return res.status(400).send({message: "Upload Failed", err: err})
+    } else if (!req.body.name) {
+      return res.status(400).send({message: "No track name found in body"})
+    }
+
+    let trackName = req.body.name
+
+    const readableTrackStream = new Readable()
+    readableTrackStream.push(req.files['track'][0].buffer)
+    readableTrackStream.push(null)
+
+    const readableImageStream = new Readable()
+    readableImageStream.push(req.files['image'][0].buffer)
+    readableImageStream.push(null)
+
+    // console.log(readableImageStream)
+
+    let bucket = new mongodb.GridFSBucket(db, {
+      bucketName: 'tracks'
+    })
+
+    let bucket2 = new mongodb.GridFSBucket(db, {
+      bucketName: 'image'
+    })
+
+    let uploadStream = bucket.openUploadStream(trackName)
+    let uploadImg = bucket2.openUploadStream(trackName)
+    let id = uploadStream.id
+    // let id = uploadImg.id
+    readableTrackStream.pipe(uploadStream)
+    readableImageStream.pipe(uploadImg)
+
+
+    uploadStream.on('error', () => {
+      return res.status(500).send({message: "Error uploading file"})
+    })
+
+    uploadStream.on('finish', () => {
+      return res.status(201).send({message: "File uploaded successfully"})
+    })
+
+  })
+})
+
+// get objectId from filename string
+
+const url = "http://localhost:8000/"
+app.get('/api/:fileName', async (req, res) => {
+  try {
+    let fileName = req.params.fileName
+
+    if (!fileName) {
+      return res.status(400).send({message: "File name not specified"})
+    }
+
+    const result = await db.collection('image.files').find({ filename: { $regex: fileName, $options: 'i' } }).toArray()
+    if (!result.length) {
+      return res.status(404).send({message: "No such file found"})
+    }
+
+    let image_ids = []
+    let file_name = []
+    result.map((item) => {
+      image_ids.push(url + "images/" + item._id.toString())
+    })
+
+    result.map((item) => {
+      file_name.push(item.filename)
+    })
+
+    const result2 = await db.collection('tracks.files').find({ filename: { $regex: fileName, $options: 'i' } }).toArray()
+    if (!result2.length) {
+      return res.status(404).send({message: "No such file found"})
+    }
+
+    let track_ids = []
+    result2.map((item) => {
+      track_ids.push(url + "tracks/" + item._id.toString())
+    })
+
+    // console.log(image_id, track_id)
+
+    return res.status(200).send({message: "Successfully Done", image_id: image_ids, track_id: track_ids, file_name: file_name})
+  } catch (err) {
+    console.log(err)
+    return res.status(500).send({message: "Internal Server Error"})
+  }
+})
+
+// get route of a song
+app.get('/tracks/:trackID', (req, res) => {
+  try {
+    var trackID = new ObjectID(req.params.trackID)
+  } catch (err) {
+    return res.status(400).send({message: "Invalid trackID in URL parameter."})
+  }
+
+  res.set('content-type', 'audio/mp3')
+  res.set('accept-ranges', 'bytes')
+
+  let bucket = new mongodb.GridFSBucket(db, {
+    bucketName: 'tracks'
+  })
+
+  let downloadStream = bucket.openDownloadStream(trackID)
+
+  downloadStream.on('data', (chunk) => {
+    res.write(chunk)
+  })
+
+  downloadStream.on('error', () => {
+    res.sendStatus(404)
+  })
+
+  downloadStream.on('end', () => {
+    res.end()
+  })
+})
+
+// get route of image
+app.get('/images/:trackID', (req, res) => {
+  try {
+    var trackID = new ObjectID(req.params.trackID)
+  } catch (err) {
+    return res.status(400).send({message: "Invalid trackID in URL parameter."})
+  }
+
+  res.set('content-type', 'image/png')
+  res.set('accept-ranges', 'bytes')
+
+  let bucket = new mongodb.GridFSBucket(db, {
+    bucketName: 'image'
+  })
+
+  let downloadStream = bucket.openDownloadStream(trackID)
+
+  downloadStream.on('data', (chunk) => {
+    res.write(chunk)
+  })
+
+  downloadStream.on('error', () => {
+    res.sendStatus(404)
+  })
+
+  downloadStream.on('end', () => {
+    res.end()
+  })
+})
+
+
+app.listen(PORT, () => console.log('Connected Successfully!', PORT))
